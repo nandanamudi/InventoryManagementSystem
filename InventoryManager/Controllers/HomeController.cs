1404441b4f4e9ca60e7f04a94f33cc856a4a2be8
kbartkuhn <bartkuhnk@gmail.com>
2016-04-29 14:48:00 -0500

    some email functionality

--- InventoryManager/Controllers/HomeController.cs
+++ InventoryManager/Controllers/HomeController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using InventoryManager.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
 
 namespace InventoryManager.Controllers
 {
@@ -25,6 +30,46 @@ namespace InventoryManager.Controllers
             ViewBag.Message = "Find us listed below.";
 
             return View();
+
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("inventory.manager@outlook.com"));  // replace with valid value 
+                message.From = new MailAddress("inventory.manager@outlook.com");  // replace with valid value
+                message.Subject = "OUt of inventory";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "inventory.manager@outlook.com",  // replace with valid value
+                        Password = "Fernando1!"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
+            return View();
+        }
+
+
     }
 }
---
+++ InventoryManager/Models/EmailFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManager.Models
+{
+        public class EmailFormModel
+        {
+            [Required, Display(Name = "Fernando")]
+            public string FromName { get; set; }
+            [Required, Display(Name = "inventory.manager@outlook.com"), EmailAddress]
+            public string FromEmail { get; set; }
+            [Required]
+            public string Message { get; set; }
+        }
+    
+    }
